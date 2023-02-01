35514be87c509e1523ef038b6c49fc70a61c3049
Joana <joanamdsantos1@gmail.com>
2023-02-01 12:16:18 -0300

    criando get, post e put no app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,59 @@
 // src/app.js
 const express = require('express');
 
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPFC',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
 const app = express();
 
+app.use(express.json());
+
+app.put('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, initials } = req.body;
+
+  const updateTeam = teams.find((team) => team.id === Number(id));
+
+  if (!updateTeam) {
+    res.status(404).json({ message: 'Team not found' });
+  }
+
+  updateTeam.name = name;
+  updateTeam.initials = initials;
+  res.status(200).json({ updateTeam });
+});
+
 app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
+
+app.get('/teams/:id', (req, res) => {
+  const { id } = req.params; 
+
+  const foundTeam = teams.find((team) => team.id === Number(id));
+
+  if (!foundTeam) {
+    res.status(404).json({ message: 'Team not found' });
+  }
+
+  res.status(200).json({ foundTeam });
+});
+
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+
+  res.status(201).json({ team: newTeam });
+});
 
 module.exports = app;
