baa135585d89e24c66308f9617641ed18efe5871
Joana <joanamdsantos1@gmail.com>
2023-02-02 14:25:36 -0300

    atualizando

--- src/teams.js
+++ src/teams.js
@@ -2,7 +2,7 @@ const fs = require('fs').promises;
 const { join } = require('path');
 
 const readTeamsFile = async () => {
-  const path = '/files/teams.json';
+  const path = '../files/teams.json';
   try {
     const contentFile = await fs.readFile(join(__dirname, path), 'utf-8');
     return JSON.parse(contentFile);
